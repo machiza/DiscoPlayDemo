392b7346862b7de7b41128404376228074139148
machiza <machizaarmando@gmail.com>
2018-02-15 14:34:28 +0200

    firebase connect

--- app/src/main/java/mz/co/hossiman/discoplaydemo/VisitActivity.java
+++ app/src/main/java/mz/co/hossiman/discoplaydemo/VisitActivity.java
@@ -1,6 +1,7 @@
 package mz.co.hossiman.discoplaydemo;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class VisitActivity extends AppCompatActivity {
 
@@ -18,29 +24,28 @@ public class VisitActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private ActionBarDrawerToggle mToggle;
     private Animation lesttoright, righttoleft;
+    private View header;
+    private NavigationView navigationView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visit);
 
-        mDrawer = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToolbar = (Toolbar) findViewById(R.id.toobarVisit);
-        mToggle = new ActionBarDrawerToggle(this,mDrawer, mToolbar, R.string.open, R.string.close);
-        mDrawer.addDrawerListener(mToggle);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        mToggle.syncState();
+        mAuth = FirebaseAuth.getInstance();
 
-        lesttoright = AnimationUtils.loadAnimation(this, R.anim.lefttoright);
-        righttoleft = AnimationUtils.loadAnimation(this, R.anim.righttoleft);
 
-//        if (OurData.velho == 0) {
-//            mDrawer.setAnimation(lesttoright);
-//            OurData.velho += 1;
-//        } else {
-//            mDrawer.setAnimation(righttoleft);
-//        }
+        initVar();
+        setHeaderNavigationView();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
     }
 
     @Override
@@ -56,6 +61,33 @@ public class VisitActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
 
         return true;
+    }
+
+    public void initVar() {
+        mDrawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToolbar = (Toolbar) findViewById(R.id.toobarVisit);
+        navigationView = (NavigationView) findViewById(R.id.navigationViewId);
+        header = navigationView.getHeaderView(0);
+
+        mToggle = new ActionBarDrawerToggle(this,mDrawer, mToolbar, R.string.open, R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        mToggle.syncState();
+    }
+
+    public void setHeaderNavigationView() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        ImageView fotoUsuario = (ImageView) header.findViewById(R.id.ivUsuarioFoto);
+        TextView nomeUsuario = (TextView) header.findViewById(R.id.txtUsuarioNome);
+        TextView emailUsuario = (TextView) header.findViewById(R.id.txtUsuarioEmail);
+
+        if (currentUser == null) {
+            fotoUsuario.setImageResource(R.drawable.default_profile);
+            nomeUsuario.setText("Utilizador convidado");
+            emailUsuario.setText("Entre ou registe-se");
+        }
+
     }
 
     public void btnMusicOnClick(View view) {
